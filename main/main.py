637e18b95ba656acc8544f322b226bf0478ecb3f
farkhodsadykov <sadykovfarkhod@gmail.com>
2018-09-29 16:16:25 -0500

    Deleted main fodler for version 2

--- main/main.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-
-myDict = dict(username='fakhodsadykov', password='redhat', email='farkhodsadykov@gmail.com', role='admin')
-with open('./test.json', 'w') as file:
-    json.dump(myDict, file, indent=2)
